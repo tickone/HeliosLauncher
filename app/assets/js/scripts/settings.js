89793bd714b93c6036ec3bf0981de101a13cdbea
Michael Kuan <michael34435@capslock.tw>
2020-08-07 09:33:25 +0800

    fix: force image to profile-01-new.png

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -870,7 +870,7 @@ function loadSelectedServerOnModsTab(){
     const serv = DistroManager.getDistribution().getServer(ConfigManager.getSelectedServer())
 
     document.getElementById('settingsSelServContent').innerHTML = `
-        <img class="serverListingImg" src="${serv.getIcon()}"/>
+        <img class="serverListingImg" src="assets/images/profile-new-01.png"/>
         <div class="serverListingDetails">
             <span class="serverListingName">${serv.getName()}</span>
             <span class="serverListingDescription">${serv.getDescription()}</span>
